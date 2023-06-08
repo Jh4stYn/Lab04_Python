d2ab9d71ddb0e2fac6d684fe71023aa72499d245
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-06-08 00:23:33 -0500

    Inicializamos y creamos nuestra ventana principal

--- Tablero.py
+++ Tablero.py
@@ -5,3 +5,10 @@ LADO_CASILLA = 80
 COLOR_CASILLA_CLARA = '#FFFFFF'
 COLOR_CASILLA_OSCURA = '#808080'
 
+# Create main window
+ventana = tk.Tk()
+ventana.title("Tablero de Ajedrez")
+
+# Create canvas
+lienzo = tk.Canvas(ventana, width=8 * LADO_CASILLA, height=8 * LADO_CASILLA)
+lienzo.pack()
