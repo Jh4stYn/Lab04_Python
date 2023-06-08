25fc2d431277f93fe64de5664d41e6a5c13ef8f9
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-06-08 00:25:01 -0500

    Graficamos nuestro tablero

--- Tablero.py
+++ Tablero.py
@@ -12,3 +12,13 @@ ventana.title("Tablero de Ajedrez")
 # Create canvas
 lienzo = tk.Canvas(ventana, width=8 * LADO_CASILLA, height=8 * LADO_CASILLA)
 lienzo.pack()
+
+# Draw the board
+for fila in range(8):
+    for columna in range(8):
+        x1 = columna * LADO_CASILLA
+        y1 = fila * LADO_CASILLA
+        x2 = x1 + LADO_CASILLA
+        y2 = y1 + LADO_CASILLA
+        color = COLOR_CASILLA_CLARA if (fila + columna) % 2 == 0 else COLOR_CASILLA_OSCURA
+        lienzo.create_rectangle(x1, y1, x2, y2, fill=color)
